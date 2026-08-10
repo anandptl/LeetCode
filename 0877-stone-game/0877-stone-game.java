c1c82a47ed0c45304d4b583f28eb8c2065c6fc2a
Anand Patel <166007508+anandptl@users.noreply.github.com>
2026-08-10 15:10:41 +0530

    Time: 8 ms (25.82%), Space: 46.9 MB (14.51%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,41 @@
+// class Solution {
+//     public boolean stoneGame(int[] piles) {
+//         return true;
+//     }
+// }
+
 class Solution {
+
     public boolean stoneGame(int[] piles) {
-        return true;
+
+        int n = piles.length;
+
+        int[][] dp = new int[n][n];
+
+        // Base case:
+        // Only one pile available
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+
+        // Length of subarray
+        for (int len = 2; len <= n; len++) {
+
+            for (int left = 0; left + len <= n; left++) {
+
+                int right = left + len - 1;
+
+                int takeLeft =
+                        piles[left] - dp[left + 1][right];
+
+                int takeRight =
+                        piles[right] - dp[left][right - 1];
+
+                dp[left][right] =
+                        Math.max(takeLeft, takeRight);
+            }
+        }
+
+        return dp[0][n - 1] > 0;
     }
 }
